df589c9162cc383e6ecd083c418855f0c47982f5
Ibrahim Fekry <ibrahimfekry159@gmail.com>
2023-05-09 16:13:19 +0300

    Muslim app shows the praying times based on your location and The holy quran and The hadith

--- lib/shared/cubit/azkar_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:adhan_app/shared/cubit/azkar_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class AzkarCubit extends Cubit<AzkarState>{
-  AzkarCubit():super(AzkarInitialState());
-
-  static AzkarCubit get(context) => BlocProvider.of(context);
-  
-}
--- lib/shared/cubit/azkar_state.dart
+++
@@ -1,9 +0,0 @@
-abstract class AzkarState{}
-
-class AzkarInitialState extends AzkarState{}
-
-class AzkarLoadingState extends AzkarState{}
-
-class AzkarLoadedState extends AzkarState{}
-
-class AzkarErrorState extends AzkarState{}
